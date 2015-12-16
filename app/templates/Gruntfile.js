8aec21f97130d43189e80d1da3c1452f653f27ac
Tri Nguyen <tri@midnightprojects.com>
2015-12-15 16:21:04 -0800

    fixed copy manifest to dist

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -302,7 +302,7 @@ module.exports = function (grunt) {
           cwd: '<%%= config.app %>',
           dest: '<%%= config.dist %>',
           src: [
-            '*.{ico,png,txt}',
+            '*.{ico,png,txt,js}',
             'images/{,*/}*.webp',
             '{,*/}*.html',
             'fonts/{,*/}*.*'
